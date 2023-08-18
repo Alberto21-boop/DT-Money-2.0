31e275475a04c13bdd330ca0518529a1ab944f0c
Alberto <albertobarbosa0003@gmail.com>
2023-08-18 10:51:35 -0300

    Implementing the frontend, installing Phorphors, and creating components.

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,32 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { PriceHightLight, TransactionsContainer, TransactionsTable } from "./styles";
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+              <PriceHightLight variant="income">R$ 12.000,00</PriceHightLight>
+              <td>Venda</td>
+              <td>13/08/2023</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hamburger</td>
+              <PriceHightLight variant="outcome">- R$ 59,90</PriceHightLight>
+              <td>Alimentação</td>
+              <td>10/08/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  );
+}
