7fd88506978d43f24d5922579d8af7d3356ea0bc
Alberto <albertobarbosa0003@gmail.com>
2023-08-22 17:10:39 -0300

    Applying Zod again

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,23 @@
 import * as Dialog from "@radix-ui/react-dialog"
-import { CloseButton, Content, Overlay, T, TransactionType, /* TransactionTypeButton  */ } from "./styles";
+import { CloseButton, Content, Overlay, T, TransactionType } from "./styles";
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import * as z from 'zod';
+import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const newTransactionFormSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  type: z.enum(['income', 'outcome']),
+})
+
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
+  const { register, handleSubmit } = useForm<NewTransactionFormInputs>({
+    resolver: zodResolver(newTransactionFormSchema)
+  });
   return (
 
     <Dialog.Portal>
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,12 +1,39 @@
+import { useForm } from "react-hook-form";
 import { SearchFormContainer } from "./styles";
 import { MagnifyingGlass } from 'phosphor-react'
+import * as z from 'zod'; // importamos o zod para utilizarmos 
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const searchFormSchema = z.object({ //em quarto criamos esta const aqui
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>; // em quinto "tipamos"
 
 export function SearchForm() {
-  return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<SearchFormInputs>(); // primeiro criamos este useForm
+  // e em sexto passamos o SearchFormInputs como generics 
+  resolver: zodResolver(searchFormSchema);
 
-      <button type="submit">
+
+  async function handleSearchTransactions(data: SearchFormInputs) { // em terceiro criamos esta função
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    console.log(data);
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input type="text"
+        placeholder="Busque por transações"
+        {...register('query')} // e em segundo adicionamos o register com um 'query'
+      />
+
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>
